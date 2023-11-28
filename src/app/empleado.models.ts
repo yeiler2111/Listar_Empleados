bf77e892c47cd54c34a166fd6e93508cc00875cb
yeiler <yeiler2209@gmail.com>
2023-11-28 18:57:13 -0500

    practica

---
+++ src/app/empleado.models.ts
@@ -0,0 +1,14 @@
+export class empleados {
+
+  constructor(nombre:string,apellido:string,cargo:string,salario:number){
+    this.nombre=nombre;
+    this.apellido=apellido;
+    this.cargo=cargo;
+    this.salario=salario;
+  }
+
+  nombre: string = "";
+  apellido: string = "";
+  cargo: string = "";
+  salario: number = 0;
+}
